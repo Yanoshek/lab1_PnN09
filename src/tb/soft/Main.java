c8da452c7d7179855ada0733158e7aa76375dd7e
janusz <ignaszkjanusz@gmail.com>
2021-10-18 10:46:07 +0200

    :boom: Created greeting text

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -2,8 +2,11 @@ package tb.soft;
 
 public class Main {
 
-    public static void main(String[] args) {
-    	System.out.println("Witaj");
-    	System.exit(0);
-    }
+	public static void main(String[] args) {
+		String witaj, imie;
+		witaj = "Witaj";
+		imie = args[0];
+		System.out.println(String.format("%s posiadaczu imienia: %s \n",witaj, imie));
+		System.exit(0);
+	}
 }
