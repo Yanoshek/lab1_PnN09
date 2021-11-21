eec102bf6104ad91916127e8939801d2c14e2e7e
Yanoshek <ignaszakjanusz@gmail.com>
2021-11-21 23:24:11 +0100

    Added multiplication table

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -7,6 +7,12 @@ public class Main {
 		witaj = "Witaj";
 		imie = args[0];
 		System.out.println(String.format("%s posiadaczu imienia: %s \n",witaj, imie));
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++) {
+				System.out.printf("%3d",i*j);
+			}
+			System.out.println("");
+		}
 		System.exit(0);
 	}
 }
